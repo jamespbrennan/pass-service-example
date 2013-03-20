58a0c68b5a7edb1a49f41adacff25fe2ac057fe5
James Brennan <james@jamesbrennan.ca>
2013-03-20 13:21:34 -0700

    Empty Rails4.0.0.beta1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PassServiceExample::Application.config.secret_key_base = '7d9cfeae533f32c998ffcc6e6867dafe023788d0846d88381efaabee62f9a02f004b2c2da99a38b56897bc6cff319d251aadf40e21c6edfe746ef46019eb2718'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PassServiceExample::Application.config.session_store :encrypted_cookie_store, key: '_pass-service-example_session'
