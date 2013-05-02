b3b9d6b38d7c1b9333f71505a6bd5c0065111397
James Brennan <james@jamesbrennan.ca>
2013-05-02 12:43:16 -0700

    Debugging session callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,19 +22,22 @@ class SessionsController < ApplicationController
   def callback
     params.required('session_id')
 
-    # begin
+    begin
       pass_session = Pass::Session.retrieve params[:session_id].to_s
 
       if pass_session.is_authenticated && pass_session.user
         session = ActiveRecord::SessionStore::Session.find_by_pass_session_id(pass_session.id)
         session.set_attribute!("pass_user_id", pass_session.user.id)
         session.save
+      else
+        logger.error 'Receieved a unauthenticated session.'
+        logger.error pass_session.to_s
       end
-    # rescue Pass::PassError => e
-    #   logger.error e.to_s
-    #   render text: 'Pass Error!'
-    #   return
-    # end
+    rescue Pass::PassError => e
+      logger.error e.to_s
+      render text: 'Pass Error!'
+      return
+    end
 
     render :nothing => true
   end
