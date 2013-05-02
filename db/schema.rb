440b07188c7c7bd7e4ef493d06de1e90e7fa4804
James Brennan <james@jamesbrennan.ca>
2013-05-01 21:12:05 -0700

    Added email column for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130502033315) do
+ActiveRecord::Schema.define(version: 20130502041122) do
 
   create_table "sessions", force: true do |t|
     t.string   "session_id",      null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20130502033315) do
     t.integer  "pass_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
