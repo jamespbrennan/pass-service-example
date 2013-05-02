ffc246228fe79593083a70b753836ac827402071
James Brennan <james@jamesbrennan.ca>
2013-05-01 17:02:03 -0700

    Fixed up users callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def callback
+    params.required(:email)
+    params.required(:id)
+    User.create(email: params[:email], pass_user_id: params[:id])
+  end
 end
