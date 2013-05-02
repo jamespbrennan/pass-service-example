028d6fdd4ebdaeb548f5ff1027eab27f5420e196
James Brennan <james@jamesbrennan.ca>
2013-05-01 17:02:48 -0700

    Fixed up routes for callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 PassServiceExample::Application.routes.draw do
   get "sessions/new"
 
+  post 'sessions/callback', to: 'sessions#callback'
+  post 'users/callback', to: 'users#callback'
+
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  post 'callback', to: 'sessions#callback'
-
-  resources :users
 
   root to: "sessions#new"
 end
